70b5ec8105b1507309b4c95b70dcde1fc2beef05
anika-001 <dream.sweeter.30.09@gmail.com>
2021-11-28 18:56:45 +0530

    initial

---
+++ src/app/JSONdata/navbar.ts
@@ -0,0 +1,27 @@
+export let navbar = [
+    {
+        "role":"user",
+        "name":"Mood Tracker",
+        "link":"/dashboard",
+    },
+    // {
+    //     "role":"user",
+    //     "name":"Home",
+    //     "link":"/home",},
+    // },
+    // {
+    //     "role":"user",
+    //     "name":"Safe Rooms",
+    //     "link":"/saferoom",
+    // },
+    // {
+    //     "role":"user",
+    //     "name":"Forum",
+    //     "link":"/forum",
+    // },
+    // {
+    //     "role":"user",
+    //     "name":"Meditate",
+    //     "link":"/meditate",
+    // }
+]
